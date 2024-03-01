93dd96d8b00121f3ec8f95b4352dd263df198224
ahmed gamal <114102777+ahmedgamaleid@users.noreply.github.com>
2024-03-01 17:20:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,180 +1,20 @@
-body {
-background-color:#fbfbfcb6  ;
-color: rgb(17, 17, 17) ;
-font-family: 'Arial' ;
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-}
-.nav{
-  color: white;
-}
-.logonav {
-    font-family: "Wavefont";
-    font-optical-sizing: auto;
-    font-weight: 400;
-    font-style: normal;
-    font-variation-settings: "ROND" 100, "YELA" -100;
-  }
-  /* home ------========================================================================================================================*/
-  .righthome {
-    text-align: right;
-    position: relative;
-  }
-  
-  .landing {
-    border-radius: 1% 1% 1% 50%;
-    width: 100%;
-    border-left: 15px solid #81b3b0;
-    
-  }
-  body{
-
-overflow-x: hidden !important;
-  }
-  .layerd {
-    color: #20B6A3;
-    background-color: rgb(253, 252, 252);
-    border-width: 5px;
-    border-style: solid; /* Add border style */
-    border-color: #cbe4e1; /* Border color */
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); /* Box shadow with color */
-    border-radius: 100%;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 70px;
-    width: 70px;
-    position: absolute;
-    left: -1%;
-    top: 40%;
-}
-
-  .closeic{
-
-color: white;
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './index.css';
 
 
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-    
-  }
-  .ico {
-    border-width: 20px;
-    border-color: aquamarine;
-  }
-  
-  .landingwri {
-    display: flex;
-    flex-direction: column;
-    text-align: left; /* Change to left for left alignment */
-    
-    justify-content: flex-start;
-    align-items: flex-start;
-}
-  
-  .fonth {
-    font-family: "Comfortaa", sans-serif;
-    font-size: 55px;
-}
-.fonthp{
-color: #20B6A3;
-font-style: normal;
-font-weight:normal;
-font-size: 13px;
-}
-
-.fontp2{
-  font-family: "Comfortaa", sans-serif;
-  font-size: 14px;
-}
-
-.font2{
-
-  font-family: "Comfortaa", sans-serif;
-}
-
-
-
-.fontb{
-  font-size: 12px;
-  background-color: #20B6A3;
-  color:white
-}
-.fontb:hover {
-  color: #20B6A3; /* Change text color to match background color on hover */
-  background-color: white; /* Change background color to white on hover */
-  border-color: #20B6A3;
-}
-.linkhome{
-text-decoration: none;
-color: #20B6A3;
-
-}
-
-.contacth{
-
-  font-family: "Comfortaa", sans-serif;
-
-
-}
-
-.servic{
-text-align: center;
-
-
-}
-
-.nameofdiv{
-text-align: center;
-
-
-}
-
-
-.fontstyle{
-
-  font-family: "Comfortaa", sans-serif;
-  
-}
-
-.lefticone {
-  color: #20B6A3;
-  background-color: #dce3e2ad;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 80px; /* Adjust width as needed */
-  height: 80px; /* Adjust height as needed */
-  border-radius: 50%; /* Make it rounded */
-}
-
-.servdiv {
-  display: flex;
-  justify-content: center; /* Center the content horizontally */
-  align-items: center; /* Center the content vertically */
-}
-
-.rightservice {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start; /* Align content to the start */
-  margin-left: 10px; /* Adjust spacing between left and right content */
-}
-
-/* Additional styles for the text elements */
-.rightservice h5,
-.rightservice p {
-  margin: 0; /* Remove default margins */
-}
-
-
-
-
-
-
-
-
-
-
-
-
+reportWebVitals();
