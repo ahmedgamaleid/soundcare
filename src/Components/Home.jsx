e9399817c8d9c892fc956e638861d60bf8e67852
ahmed gamal <114102777+ahmedgamaleid@users.noreply.github.com>
2024-03-22 08:11:34 +0200

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import landing from "../img/landing2.jpg"
 import doctor from '../../src/img/doctor.jpg'
-import dr from '../../src/img/dr.jpg'
+
 const Home = () => {
   return (
     // ============================================================ start home ================================================================
@@ -77,66 +77,7 @@ const Home = () => {
         </div>
     </div>
 
-    {/* ===================================================== about ================================================================================== */}
-    <div className='container my-5 d-flex justify-content-between g-4 '>
-  <style>
-    @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap');
-  </style>
-  <div className='row d-flex justify-content-between g-4'>
-    <div className='col-lg-6'>
-      <div className="row">
-        <div className='col-lg-12 drimg'>
-          <img src={dr} className='w-100 h-100 rounded-4' />
-          <div className='childdr text-center  rounded-cycle '> 
-            <h1>25+</h1>
-            <spam className='spamst'>YEARS OF EXPERIENCE</spam>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className='col-lg-6'>
-      <div className="row">
-        <div className='col-lg-12 rightabout mx-5'>
-          <header className='headerco my-4'>ABOUT US</header>
-          <h1 className='my-3'>We Are Professional <br></br> Diagnosis & Medical <br></br>Service</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, minus provident ex aperiam officiis minima temporibus, ea obcaecati voluptatum, dolorem ab porro rem ducimus id doloremque perspiciatis voluptates! Obcaecati, facilis.</p>
-          <button className='btn btnn main-color fontb rounded-5  py-2 '>Discover more </button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-{/* ===================================================== about ================================================================================== */}
-
-
-<div className='container fontstyle my-3 d-flex justify-content-center text-center my-5'>
-  <div className="col-md-4 second-color brdr about">
-    <div className='py-3 ps-5'>
-      <i className="fa-solid fa-user-doctor icon"></i>
-      <h3 className='about-h'>Personalized Health Consultations</h3>
-      <p className='about-p'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit reiciendis error dolorem. Laudantium consectetur omnis, fugit incidunt cupiditate facilis id.</p>
-    </div>
-  </div>
-  <div className="col-md-4 second-color brdr ms-5 about">
-    <div className='py-3 ps-5'>
-      <i className="fa-solid fa-heart-pulse icon"></i>
-      <h3 className='about-h'>Personalized Health Consultations</h3>
-      <p className='about-p'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit reiciendis error dolorem. Laudantium consectetur omnis, fugit incidunt cupiditate facilis id.</p>
-    </div>
-  </div>
-  <div className="col-md-4 second-color brdr ms-5 about about-th">
-    <div className='py-3 ps-5'>
-      <i className="fa-solid fa-heart-pulse icon"></i>
-      <h3 className='about-h'>Personalized Health Consultations</h3>
-      <p className='about-p'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit reiciendis error dolorem. Laudantium consectetur omnis, fugit incidunt cupiditate facilis id.</p>
-    </div>
-  </div>
-</div>
-
-
+  
 {/*-========================================================================================================================= */}
 <div className='countainer  fontstyle my-5 '>
   <div className='nameofdiv'> <p className='fonthp'>WHAT WE OFFER</p> <h1>Best services available for the<br></br> best customers</h1><p>Don't settle for ordinary. Elevate your well-being with the extraordinary benefits of <br></br>the soundcare project</p> </div>
@@ -218,8 +159,8 @@ const Home = () => {
 <div className="home fontstyle container d-flex justify-content-between my-5 bg-white h-100  " >
         <div className='col-md-4 my-5 first'>
           <header>OUR SERVICES</header>
-          <h3>Healminos labs</h3>
-          <p>Enhance your quality of life with Healminos Project! Don't miss the chance
+          <h3>sound care  site</h3>
+          <p>Enhance your quality of life with sound care Project! Don't miss the chance
             to have access to innovative healthcare services that will help you achieve better well-doing <br />
             <br /> 
             <button className='btn btnn  fontb rounded-5  py-2 '>Discover more </button>
