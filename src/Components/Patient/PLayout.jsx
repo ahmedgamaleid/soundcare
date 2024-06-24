57244d452c078e7246b9885e238b831b8c4a8e0b
ahmed gamal <114102777+ahmedgamaleid@users.noreply.github.com>
2024-06-24 14:11:07 +0200

    Update PLayout.jsx

--- src/Components/Patient/PLayout.jsx
+++ src/Components/Patient/PLayout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import PNavbar from './PNavbar'
 import { Outlet } from 'react-router-dom'
-
+import Footer from '../admin/Footer'
 export default function PLayout({children}) {
   return (
     <>
       <PNavbar/>
       <Outlet/>
       {children}
+      <Footer/>
     </>
   )
 }
